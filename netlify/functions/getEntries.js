bdc8ef0b82c8ca12d0dea3766cf11e9978a4787b
amtic50 <amitchoudhury15@gmail.com>
2025-09-13 13:43:04 +0530

    Update getEntries.js

--- netlify/functions/getEntries.js
+++ netlify/functions/getEntries.js
@@ -1,7 +1,5 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
 console.log('google-spreadsheet version:', require('google-spreadsheet/package.json').version);  
-
-
 export async function handler(event, context) {
   try {
     const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
